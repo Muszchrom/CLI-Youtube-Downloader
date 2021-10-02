ce1c7a4a8e90d8e9cfd9d0d986715193772278e3
Karol Chabros <altair2468@o2.pl>
2021-10-02 17:30:52 +0200

    Optimised stuff and fixed bugs

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ import os
 import re
 
 
-def help_command():
+def input_link_loop(format_type):
+    while True:
+        link = input('Link/exit ~ ')
+        if link == 'exit':
+            return
+        else:
+            download_file(format_type, link)
+
+
+def help_command(advanced=False):
+    if advanced:
+        print('=====================================')
+        print('advanced commands')
+        print('streams - display all streams')
+        print('itag - (in video case) get by itag (only for mp4 and progressive)')
+        print('=====================================')
+        return
     print('=====================================')
     print('commands')
     print('exit - exit')
@@ -15,11 +31,10 @@ def help_command():
     print('=====================================')
 
 
-def advanced_help_command():
-    print('=====================================')
-    print('advanced commands')
-    print('streams - display all streams')
-    print('itag - (in video case) get by itag (only for mp4 and progressive)')
+def print_streams(yt):
+    print('=====================================\nPrinting streams')
+    for stream in yt.streams:
+        print(stream)
     print('=====================================')
 
 
@@ -27,7 +42,7 @@ def check_link(link):
     try:
         return YouTube(link)
     except exceptions.RegexMatchError:
-        return False
+        return
 
 
 def get_qualities(yt, format_type):
@@ -35,21 +50,21 @@ def get_qualities(yt, format_type):
     re_res = '[0-9]{3,}p'
     re_audio = '[0-9]{2,}kbps'
 
+    print(yt.streams)
+
     if format_type == 'video':
         for stream in yt.streams.filter(file_extension="mp4"):
-            try:
-                quality, itag = get_qualities_helper(re_res, qualities, str(stream))
+            x = get_qualities_helper(re_res, qualities, str(stream))
+            if x:
+                quality, itag = x
                 qualities[quality] = itag
-            except TypeError:
-                pass
 
     elif format_type == 'audio':
         for stream in yt.streams:
-            try:
-                quality, itag = get_qualities_helper(re_audio, qualities, str(stream))
+            x = get_qualities_helper(re_audio, qualities, str(stream))
+            if x:
+                quality, itag = x
                 qualities[quality] = itag
-            except TypeError:
-                pass
 
     return qualities
 
@@ -66,9 +81,9 @@ def get_qualities_helper(re_quality, qualities, stream):
         itag = itag[6:-1]
 
         if quality in qualities:
-            pass
+            return
         else:
-            return quality, itag
+            return [quality, itag]
 
 
 def add_audio_to_video(audio_path, video_path, output_path):
@@ -77,110 +92,110 @@ def add_audio_to_video(audio_path, video_path, output_path):
     clip = VideoFileClip(video_path)
     audio = AudioFileClip(audio_path)
     videoclip = clip.set_audio(audio)
+    if not os.path.exists('downloaded'):
+        os.mkdir('downloaded')
     videoclip.write_videofile(output_path)
     print('=====================================')
     print('Done!')
 
 
 def download_file(format_type, link, just_audio=True):
+    yt = check_link(link)
+    if not yt:
+        print('Invalid link')
+        return
 
-        yt = check_link(link)
-        if not yt:
-            print('Invalid link')
+    quality = get_qualities(yt, format_type)
+    if not quality:
+
+        return
+    displayed_qualities = []
+    for key in quality:
+
+        displayed_qualities.append(key)
+    print(f'*************************************\n{yt.title}\n*************************************')
+    print(f'quality: {displayed_qualities}')
+
+    while True:
+
+        chosen_quality = input('Choose quality/exit/advanced ~ ')
+        if chosen_quality == 'exit':
             return
 
-        quality = get_qualities(yt, format_type)
-        if not quality:
-            return
+        elif chosen_quality == 'streams':
+            print_streams(yt)
 
-        displayed_qualities = []
-        for key in quality:
-            displayed_qualities.append(key)
+        elif chosen_quality == 'advanced':
+            help_command(advanced=True)
 
-        print(f'*************************************\n{yt.title}\n*************************************')
-        print(f'quality: {displayed_qualities}')
+        elif chosen_quality == 'itag':
+            print('Command under development')
+            pass
+            # try:
+            #     print(yt.streams.get_by_itag(int(chosen_quality)))
+            #     if input('Download this stream? y/n: ').lower() == 'y':
+            #         yt.streams.get_by_itag(int(chosen_quality)).download()
+            #         print('Stream downloaded!')
+            #         return
+            #     else:
+            #         return
+            # except KeyError:
+            #     print('Invalid quality')
 
-        while True:
+        else:
+            try:
+                print(yt.streams.get_by_itag(quality[chosen_quality]))
+                if input('Download this stream? y/n ~ ').lower() == 'y':
+                    if format_type == 'video':
+                        print('Downloading video...')
+                        if yt.streams.get_by_itag(quality[chosen_quality]).is_progressive:
+                            yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="downloaded")
+                            print('Video downloaded!')
+                            return
 
-            chosen_quality = input('Choose quality/exit/advanced: ')
-            if chosen_quality == 'exit':
-                break
+                        yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="temp/video")
+                        print('Video downloaded!')
 
-            elif chosen_quality == 'streams':
-                print('=====================================\nPrinting streams')
-                for stream in yt.streams:
-                    print(stream)
-                print('=====================================')
+                        print('!Video has no audio, must download it separately')
+                        download_file('audio', link, just_audio=False)
 
-            elif chosen_quality == 'advanced':
-                advanced_help_command()
+                        title = yt.streams.get_by_itag(quality[chosen_quality]).title
+                        audio_path = f'temp/audio/{os.listdir("temp/audio")[0]}'
+                        video_path = f'temp/video/{os.listdir("temp/video")[0]}'
+                        final_path = f'downloaded/{title}.mp4'
 
-            elif chosen_quality == 'itag':
-                try:
-                    print(yt.streams.get_by_itag(int(chosen_quality)))
-                    if input('Download this stream? y/n: ').lower() == 'y':
-                        yt.streams.get_by_itag(int(chosen_quality)).download()
-                        print('Stream downloaded!')
-                        break
-                    else:
-                        break
-                except KeyError:
-                    print('Invalid quality')
-
-            else:
-                try:
-                    print(yt.streams.get_by_itag(quality[chosen_quality]))
-                    if input('Download this video? y/n: ').lower() == 'y':
-                        if format_type == 'video':
-                            yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="temp/video")
-                            print('Video downloaded')
-                            title = yt.streams.get_by_itag(quality[chosen_quality]).title
-                            if not yt.streams.get_by_itag(quality[chosen_quality]).is_progressive:
-                                print('Video has no audio, must download it separately')
-                                download_file('audio', link, just_audio=False)
-
-                                video_path_mp4 = f'temp/video/{title}.mp4'
-                                audio_path_mp3 = f'temp/audio/{title}.mp3'
-                                audio_path_webm = f'temp/audio/{title}.webm'
-                                final_path = f'downloaded/{title}.mp4'
-
-                                if os.path.exists(video_path_mp4):
-                                    if os.path.exists(audio_path_mp3):
-                                        add_audio_to_video(audio_path_mp3, video_path_mp4, final_path)
-                                        os.remove(audio_path_mp3)
-                                    elif os.path.exists(audio_path_webm):
-                                        add_audio_to_video(audio_path_webm, video_path_mp4, final_path)
-                                        os.remove(audio_path_webm)
-                                    else:
-                                        print(f'Could not find file - {audio_path_mp3} / {audio_path_webm}')
-                                    os.remove(video_path_mp4)
-                                else:
-                                    print(f'Could not find file - {video_path_mp4}')
-
-                                break
+                        if os.path.exists(video_path):
+                            if os.path.exists(audio_path):
+                                add_audio_to_video(audio_path, video_path, final_path)
+                                os.remove(audio_path)
                             else:
-                                path = f'temp/video/{title}.mp4'
-                                if os.path.exists(path):
-                                    os.replace(path, f'downloaded/{title}.mp4')
-                                break
+                                print(f'Could not find file - {audio_path}')
+                            os.remove(video_path)
+                        else:
+                            print(f'Could not find file - {video_path}')
 
-                        elif format_type == 'audio':
-                            if just_audio:
-                                yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="downloaded")
-                            else:
-                                yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="temp/audio")
+                    elif format_type == 'audio':
+                        print('Downloading audio...')
+                        if just_audio:
+                            yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="temp/audio")
+                            print('Audio downloaded\nFixing extension...')
+                            audio_wrong_format = AudioFileClip(f'temp/audio/{os.listdir("temp/audio")[0]}')
+                            audio_wrong_format.write_audiofile(f'downloaded/{yt.streams.get_by_itag(quality[chosen_quality]).title}.mp3')
+                            os.remove(f'temp/audio/{os.listdir("temp/audio")[0]}')
+                            print('Audio downloaded and saved as mp3!')
+                        else:
+                            yt.streams.get_by_itag(quality[chosen_quality]).download(output_path="temp/audio")
                             print('Audio downloaded!')
-                            break
-                    else:
-                        break
-                except KeyError:
-                    print('Invalid quality')
+                return
+            except KeyError:
+                print('Invalid quality')
 
 
 print('Welcome to YouTube downloader')
 print('To start working just paste link to video or playlist')
 print('Type "help" for commands')
 print('Type "exit" to exit')
+
 while True:
     command = input('~ ')
 
@@ -199,17 +214,7 @@ while True:
             print('KeyError')
 
     elif command == 'video':
-        while True:
-            link = input('Link/exit ~ ')
-            if link == 'exit':
-                break
-            else:
-                download_file('video', link)
+        input_link_loop(command)
 
     elif command == 'audio':
-        while True:
-            link = input('Link/exit ~ ')
-            if link == 'exit':
-                break
-            else:
-                download_file('audio', link)
+        input_link_loop(command)
